ae6ffc83f9ff54581b7119695fc929cbe0fde6e5
Khushi <khushidubey422004@gmail.com>
2025-02-01 08:57:44 +0530

    Changed the rules in eslintrc.json

--- src/components/Sound.jsx
+++ src/components/Sound.jsx
@@ -52,9 +52,9 @@ const Sound = () => {
     );
   }, []);
 
-    useEffect(() => {
-      handleFirstUserInteraction();
-  }, [handleFirstUserInteraction]);  
+  useEffect(() => {
+    handleFirstUserInteraction();
+  }, [handleFirstUserInteraction]);
 
   const toggle = () => {
     const newState = !isPlaying;
